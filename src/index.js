4d990b9701e8e09ec90f59e7bce296dd68be9437
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-21 09:01:37 -0400

    Fetch dog images and display them on the DOM

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
+const dogImgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+const dogBreedSection = document.getElementById("dog-breeds")
+
+const getDogImgs = () => {
+  fetch(dogImgUrl)
+  .then(resp => resp.json())
+  .then(resp => displayDogImgs(resp))
+}
+
+const displayDogImgs = (resp) => {
+  const imgArray = resp.message
+  imgArray.forEach(dogImg => {
+    const imgSection = document.getElementById("dog-image-container")
+    const imgTag = document.createElement("img")
+    imgTag.src = dogImg
+    imgSection.appendChild(imgTag)
+    });
+}
+
 
 const init = () => {
-  console.log('%c HI', 'color: firebrick')
+  getDogImgs()
 
-
-
-
-
+  fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(resp => {
+    const dogBreedObject = resp.message
+    console.log(dogBreedObject)
+    for (const dogBreed in dogBreedObject){
+      if(dogBreedObject[dogBreed].length >= 1){
+        
+      }
+    }
+  })
 
 
 }
